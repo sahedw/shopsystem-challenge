22c425fd744fddf192c3d16e99d45d7d3b7de4dd
Sahed Wahedi <sahed.w@hotmail.de>
2023-06-11 21:14:35 +0200

    test github actions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ShopSystem shop = new ShopSystem();
-
         System.out.println(shop.listOrders());
+        System.out.println(shop.getProduct("2"));
     }
 }
