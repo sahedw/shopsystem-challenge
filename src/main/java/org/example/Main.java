18fd4bbcce6204269a7c0bbbdc2712e187905501
Sahed Wahedi <sahed.w@hotmail.de>
2023-06-11 21:17:10 +0200

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import org.example.model.ShopSystem;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
